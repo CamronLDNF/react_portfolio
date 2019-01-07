b7c3d5a456b7f36455629a62bc8209be8b15ee49
camron <camronmoradi@gmail.com>
2019-01-07 21:03:52 +0100

    Creates project cards, adds Undraw illustrations, updates json data file

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import ProjectCard from './ProjectCard'
+import { UndrawDashboard } from 'react-undraw-illustrations';
 
 class Projects extends Component {
   constructor() {
@@ -25,19 +27,28 @@ class Projects extends Component {
     if (projects.length > 0) {
       projectsList = projects.map(project => {
         return (
-          <div key={project.id}>
-            <h3 className='content=text'>
-              {project.name}
-            </h3>
+          <div key={project.id} className="min-h-900 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+            <ProjectCard project={project} />
           </div>
         )
       })
     }
 
     return (
-      <div>
-        <h1 className='content-text'>My Projects</h1>
-        {projectsList}
+      <div className="content-wrapper">
+        <div className="flex mb-4">
+          <div className="w-1/4">
+            <UndrawDashboard primaryColor='#12283a' height='200px' />
+          </div>
+          <div className="w-3/4">
+            <h1>My Projects</h1>
+            <p>This is a selection of some of my projects I have been working on.</p>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-1 lg:-mx-4">
+          {projectsList}
+        </div>
       </div>
     )
   }
